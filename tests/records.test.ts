4098d35b813f36584c8928f667ec6575a70e3c3a
Marcello <marcello@roark.at>
2024-03-24 14:36:25 +0100

    update dotenv import

--- tests/records.test.ts
+++ tests/records.test.ts
@@ -1,11 +1,11 @@
-import { config } from 'dotenv';
-config();
+import 'dotenv/config';
 import { expect, test } from 'vitest';
 
 import Airtable from '../lib/main';
 
 async function runTests() {
   const airtable = new Airtable();
+
   const apiKey = process.env.API_KEY;
   if (!apiKey) throw new Error('API_KEY .env not found');
   airtable.auth(apiKey);
