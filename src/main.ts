623bc59bf66e0084fd8a8d67b09aec07af114eef
Marcello <marcello@roark.at>
2024-03-24 14:36:11 +0100

    validate inputs

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,44 @@ type AirtableRecord = {
 };
 
 export default class Airtable {
-  private apiKey: string | undefined;
-  private baseId: string | undefined;
-  private tableId: string | undefined;
-  private readonly apiURL: string = 'https://api.airtable.com/v0';
+  private apiKey: string | undefined = undefined;
+  private baseId: string | undefined = undefined;
+  private tableId: string | undefined = undefined;
+  private apiURL: string = 'https://api.airtable.com/v0';
+
+  url(url: string): this {
+    if (typeof url === 'string') {
+      this.apiURL = url;
+    } else {
+      throw new Error('API URL must be a string');
+    }
+    return this;
+  }
 
   auth(key: string): this {
-    this.apiKey = key;
+    if (typeof key === 'string') {
+      this.apiKey = key;
+    } else {
+      throw new Error('API Key must be a string');
+    }
     return this;
   }
 
   base(baseId: string): this {
-    this.baseId = baseId;
+    if (typeof baseId === 'string') {
+      this.baseId = baseId;
+    } else {
+      throw new Error('baseId must be a string');
+    }
     return this;
   }
 
   table(tableId: string): this {
-    this.tableId = tableId;
+    if (typeof tableId === 'string') {
+      this.tableId = tableId;
+    } else {
+      throw new Error('tableId/tableName must be a string');
+    }
     return this;
   }
 
@@ -36,10 +57,14 @@ export default class Airtable {
     method: RequestMethods;
     body?: object;
   }): Promise<unknown> {
-    if (!this.apiKey || !this.baseId || !this.tableId) {
-      throw new Error(
-        'API Key, Base ID, and Table ID/Name must be set before making requests.'
-      );
+    if (!this.apiKey) {
+      throw new Error('API Key must be set before making requests.');
+    }
+    if (!this.baseId) {
+      throw new Error('Base ID must be set before making requests.');
+    }
+    if (!this.tableId) {
+      throw new Error('Table ID/Name must be set before making requests.');
     }
     return await apiRequest({
       url: `${this.apiURL}/${this.baseId}/${this.tableId}${endpoint}`,
