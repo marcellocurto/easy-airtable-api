3d39bf5bd65eea6896535609301ba915774d10e6
Marcello <marcello@roark.at>
2024-11-05 14:14:16 +0100

    restore option params

--- src/records.ts
+++ src/records.ts
@@ -31,25 +31,43 @@ export async function getRecords<Fields>({
   apiKey,
   baseId,
   tableId,
-  ...options
+  options,
 }: {
   apiKey: string;
   baseId: string;
   tableId: string;
-} & GetRecordsQueryParameters): Promise<AirtableRecord<Fields>[]> {
-  const validatedOptions = validateGetRecordsOptions(options);
-  return await airtableRequest<AirtableRecord<Fields>[]>({
-    apiKey,
-    baseId,
-    tableId,
-    endpoint: '',
-    method: 'GET',
-    body: validatedOptions,
-  });
+  options?: GetRecordsQueryParameters;
+}): Promise<AirtableRecord<Fields>[]> {
+  validateGetRecordsOptions(options);
+  let records: AirtableRecord<Fields>[] = [];
+  let currentOffset: string | undefined;
+  do {
+    const requestBody = currentOffset
+      ? { ...options, offset: currentOffset }
+      : options;
+    const response = await airtableRequest<{
+      records: AirtableRecord<Fields>[];
+      offset?: string;
+    }>({
+      apiKey,
+      baseId,
+      tableId,
+      endpoint: '/listRecords',
+      method: 'POST',
+      body: requestBody,
+    });
+    records = records.concat(response.records);
+    currentOffset = response.offset;
+    if (currentOffset) await delay(options?.requestInterval ?? 500);
+  } while (currentOffset);
+
+  return records;
 }
 
-export function validateGetRecordsOptions(options: GetRecordsQueryParameters) {
+function validateGetRecordsOptions(options?: GetRecordsQueryParameters) {
   const validatedOptions: GetRecordsQueryParameters = {};
+
+  if (!options) return validatedOptions;
 
   if (options.fields) {
     validatedOptions.fields = options.fields;
@@ -118,22 +136,58 @@ export async function updateRecords<Fields>({
   baseId,
   tableId,
   records,
-  typecast,
+  options,
 }: {
   apiKey: string;
   baseId: string;
   tableId: string;
-  records: { id: string; fields: Partial<Fields> }[];
-  typecast?: boolean;
-}): Promise<AirtableRecord<Fields>[]> {
-  return await airtableRequest<AirtableRecord<Fields>[]>({
-    apiKey,
-    baseId,
-    tableId,
-    endpoint: '',
-    method: 'PATCH',
-    body: { records, typecast },
-  });
+  records: { id: string; fields: Fields }[];
+  options?: {
+    typecast?: boolean;
+    returnFieldsByFieldId?: boolean;
+    overwriteFieldsNotSpecified?: boolean;
+    requestInterval?: number;
+  };
+}): Promise<{
+  records: AirtableRecord<Fields>[];
+}> {
+  if (!Array.isArray(records) || records.length === 0) {
+    throw new Error(
+      'The records array is empty or not provided. Please provide a non-empty array of records to update.'
+    );
+  }
+
+  const chunkSize = 10;
+  const chunks = [];
+  for (let i = 0; i < records.length; i += chunkSize) {
+    chunks.push(records.slice(i, i + chunkSize));
+  }
+
+  let combinedResults: AirtableRecord<Fields>[] = [];
+
+  for (const chunk of chunks) {
+    const result = await airtableRequest<{
+      createdRecords: string[];
+      updatedRecords: string[];
+      records: AirtableRecord<Fields>[];
+    }>({
+      apiKey,
+      baseId,
+      tableId,
+      endpoint: '/',
+      method: options?.overwriteFieldsNotSpecified === true ? 'PUT' : 'PATCH',
+      body: {
+        records: chunk,
+        typecast: options?.typecast ?? false,
+        returnFieldsByFieldId: options?.returnFieldsByFieldId ?? false,
+      },
+    });
+
+    combinedResults = combinedResults.concat(result.records);
+    await delay(options?.requestInterval ?? 500);
+  }
+
+  return { records: combinedResults };
 }
 
 export async function updateRecordsUpsert<Fields>({
@@ -166,33 +220,127 @@ export async function deleteRecords({
   baseId,
   tableId,
   recordIds,
+  options,
 }: {
   apiKey: string;
   baseId: string;
   tableId: string;
   recordIds: string[];
+  options?: { requestInterval?: number };
 }): Promise<DeleteRecordsResponse> {
-  if (recordIds.length === 1) {
-    const response = await airtableRequest<DeleteRecordResponse>({
+  if (!Array.isArray(recordIds) || recordIds.length === 0) {
+    throw new Error(
+      'The record ids array is empty or not provided. Please provide a non-empty array of record ids to delete the records.'
+    );
+  }
+
+  const chunkSize = 10;
+  const chunks = [];
+  for (let i = 0; i < recordIds.length; i += chunkSize) {
+    chunks.push(recordIds.slice(i, i + chunkSize));
+  }
+
+  let combinedResults: DeleteRecordResponse[] = [];
+
+  for (const chunk of chunks) {
+    const query = chunk.map((id) => `records[]=${id}`).join('&');
+    const result = await airtableRequest<DeleteRecordsResponse>({
       apiKey,
       baseId,
       tableId,
-      endpoint: `/${recordIds[0]}`,
+      endpoint: `?${query}`,
       method: 'DELETE',
     });
-    return {
-      records: [response],
-    };
+
+    combinedResults = combinedResults.concat(result.records);
+    await delay(options?.requestInterval ?? 500);
   }
 
-  return await airtableRequest<DeleteRecordsResponse>({
+  return { records: combinedResults };
+}
+
+export async function createRecord<Fields>({
+  apiKey,
+  baseId,
+  tableId,
+  fields,
+  options,
+}: {
+  apiKey: string;
+  baseId: string;
+  tableId: string;
+  fields: Fields;
+  options?: {
+    typecast?: boolean;
+    returnFieldsByFieldId?: boolean;
+  };
+}): Promise<AirtableRecord<Fields>> {
+  return await airtableRequest<AirtableRecord<Fields>>({
     apiKey,
     baseId,
     tableId,
     endpoint: '',
-    method: 'DELETE',
+    method: 'POST',
     body: {
-      records: recordIds,
+      fields,
+      typecast: options?.typecast ?? false,
+      returnFieldsByFieldId: options?.returnFieldsByFieldId ?? false,
     },
   });
+}
+
+export async function createRecords<Fields>({
+  apiKey,
+  baseId,
+  tableId,
+  records,
+  options,
+}: {
+  apiKey: string;
+  baseId: string;
+  tableId: string;
+  records: { fields: Fields }[];
+  options?: {
+    typecast?: boolean;
+    returnFieldsByFieldId?: boolean;
+    requestInterval?: number;
+  };
+}): Promise<{
+  records: AirtableRecord<Fields>[];
+}> {
+  if (!Array.isArray(records) || records.length === 0) {
+    throw new Error(
+      'The records array is empty or not provided. Please provide a non-empty array of records to create.'
+    );
+  }
+
+  const chunkSize = 10;
+  const chunks = [];
+  for (let i = 0; i < records.length; i += chunkSize) {
+    chunks.push(records.slice(i, i + chunkSize));
+  }
+
+  let combinedResults: AirtableRecord<Fields>[] = [];
+
+  for (const chunk of chunks) {
+    const result = await airtableRequest<{
+      records: AirtableRecord<Fields>[];
+    }>({
+      apiKey,
+      baseId,
+      tableId,
+      endpoint: '',
+      method: 'POST',
+      body: {
+        records: chunk,
+        typecast: options?.typecast ?? false,
+        returnFieldsByFieldId: options?.returnFieldsByFieldId ?? false,
+      },
+    });
+    combinedResults = combinedResults.concat(result.records);
+  }
+
+  return {
+    records: combinedResults,
+  };
 }
